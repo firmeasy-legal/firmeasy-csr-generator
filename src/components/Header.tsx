f53928c9946dceecb1e398c5cedf704449a75c3f
Gustavo A. <gustavo.atencio@outlook.com>
2024-10-24 12:42:09 -0500

    add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,11 +8,14 @@ export function Header({ }: Props) {
 	const { theme } = useTheme()
 
 	return (
-		<header className="w-full p-4">
+		<header className="w-full p-6 flex gap-4 items-center box-border" >
 			<a href="https://girasol.pe" target="_blank">
-				<img src="/logo-girasol.png" alt="Tauri Logo" className="h-10 transition-[filter] duration-300" style={
+				<img src="/logo-girasol.png" alt="Logo Girasol PE" className="block h-12 transition-[filter] duration-300" style={
 					theme !== "light" ? { filter: "brightness(0) invert(1)" } : {}} />
 			</a>
+			<picture className="block">
+				<img src="/verified-by-sunat.svg" className="h-7" alt="Verificados por SUNAT" />
+			</picture>
 		</header>
 	)
 }
