1054db1862f07f978235abb5e73268dd95a4894e
Gustavo A. <gustavo.atencio@outlook.com>
2024-10-25 19:04:57 -0500

    add inputs for csr

--- src/components/GenerateCSRForm.tsx
+++ src/components/GenerateCSRForm.tsx
@@ -1,11 +1,35 @@
 import { invoke } from "@tauri-apps/api/core";
 import { Button } from "./ui/button"
-import { KeyIcon, LoaderIcon, ZapIcon } from "lucide-react";
-import { Textarea } from "./ui/textarea";
+import { LoaderIcon, ZapIcon } from "lucide-react";
 import { Dispatch, SetStateAction, useState } from "react";
 import { toast } from "sonner";
 import { PrivateKeyWithCSR } from "@/models";
 import { cn } from "@/lib/utils";
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { z } from "zod"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+
+type CSRGenerationParams = {
+	country: string,
+	state: string,
+	locality: string,
+	organizationUnit: string,
+	commonName: string,
+}
+
+const formSchema = z.object({
+	country: z.string().length(2),
+	region: z.string(),
+	province: z.string(),
+	district: z.string(),
+	organizationUnit: z.string(),
+	commonName: z.string(),
+})
+
 
 type GeneratedPrivateKeyWithCSR = {
 	base64_private_key_pem: string,
@@ -28,6 +52,18 @@ export function GenerateCSRForm({
 	//! useTransition doesn't work with Tauri yet
 	// const [isGenerating, startGeneration] = useTransition();
 	const [isGenerating, setIsGenerating] = useState(false);
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			country: "PE",
+			region: "",
+			province: "",
+			district: "",
+			organizationUnit: "",
+			commonName: ""
+		},
+	})
 
 	async function generateCSR() {
 		setIsGenerating(true);
@@ -59,10 +95,97 @@ export function GenerateCSRForm({
 		setIsGenerating(false);
 	}
 
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values)
+	}
+
 	return (
-		<form className={cn(className, "")}>
-			<div className="flex justify-between mb-6">
+		<Form {...form} >
+			<form className={cn(className, "space-y-4")}>
+				<div className="grid grid-cols-3 gap-6">
+					<FormField
+						control={form.control}
+						name="country"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>País</FormLabel>
+								<FormControl>
+									<Input {...field} value="PE" readOnly />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="region"
+						render={({ field }) => (
+							<FormItem className="col-span-2">
+								<FormLabel>Región</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder="Lima" />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<FormField
+					control={form.control}
+					name="province"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Provincia</FormLabel>
+							<FormControl>
+								<Input {...field} placeholder="Lima" />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="district"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Distrito</FormLabel>
+							<FormControl>
+								<Input {...field} placeholder="San Isidro" />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="organizationUnit"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Unidad Organizacional</FormLabel>
+							<FormControl>
+								<Input {...field} placeholder="Contabilidad" />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="commonName"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Nombre Común</FormLabel>
+							<FormControl>
+								<Input {...field} placeholder="FirmEasy" />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
 				<Button
+					type="submit"
 					disabled={isGenerating}
 					className="flex px-6 dark:text-gray-100 hover:dark:text-gray-50 select-none"
 					onClick={generateCSR}
@@ -74,7 +197,7 @@ export function GenerateCSRForm({
 					}
 					<span className="block text-lg leading-6">Generar CSR</span>
 				</Button>
-			</div>
-		</form>
+			</form>
+		</Form>
 	)
 }
--- src/routes/generate-csr-page.tsx
+++ src/routes/generate-csr-page.tsx
@@ -8,7 +8,7 @@ export function GenerateCSRPage() {
 	const [privateKeyWithCSR, setPrivateKeyWithCSR] = useState<PrivateKeyWithCSR | null>(null)
 
 	return (
-		<Layout className="grid gap-2 grid-cols-5">
+		<Layout className="grid gap-10 grid-cols-5">
 			<GenerateCSRForm
 				className="col-span-2"
 				setPrivateKeyWithCSR={setPrivateKeyWithCSR}
