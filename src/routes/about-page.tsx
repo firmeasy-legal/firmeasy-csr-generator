d5ef7521e7be99e0ec67e8ce0b80359410c33bb8
Gustavo A <gustavo.atencio@outlook.com>
2024-10-28 16:32:20 -0500

    add about page

--- src/routes/about-page.tsx
+++ src/routes/about-page.tsx
@@ -2,8 +2,39 @@ import { Layout } from "@/layout";
 
 export function AboutPage() {
 	return (
-		<Layout>
-			About
+		<Layout className="max-w-3xl ml-0">
+			<h1 className="text-2xl font-bold mb-4">
+				Girasol CSR Generator
+			</h1>
+
+			<p className="">
+				<span className="font-semibold">Versión: </span>
+				<span>1.0.0</span>
+			</p>
+
+			<p className="">
+				<span className="font-semibold">Fecha de Lanzamiento: </span>
+				<span>2024-11-01</span>
+			</p>
+
+			<p className="mb-4">
+				<span className="font-semibold">Código fuente: </span>
+				<a
+					className="text-blue-500 dark:text-blue-600 underline visited:text-purple-500 dark:visited:text-purple-400 group"
+					href="https://github.com/firmeasy-legal/firmeasy-csr-generator"
+					target="_blank"
+				>
+					github.com
+				</a>
+			</p>
+
+			<div className="mb-6">
+				<p className="font-semibold mb-1">Descripción </p>
+				<p className="text-balance mb-2">Esta es una aplicación de código abierto de desarrollada por Girasol PE E.I.R.L que facilita a sus clientes para generar un CSR para solicitar un certificado de agente automatizado de la autoridad de certificación FirmEasy.</p>
+				<p className="text-balance">
+					Esta aplicación no envía ninguna información a servidores externos.
+				</p>
+			</div>
 		</Layout>
 	)
 }
